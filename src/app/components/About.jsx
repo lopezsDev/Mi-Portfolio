847285a937f71a21051e70106e660c69e2794ff5
LopezS7 <samirisraellopez21@gmail.com>
2024-05-23 13:38:26 -0600

    Se crean la pantalla principal, y se definen las siguientes secciones para saber mejor como voy creando las estructuras de componentes

---
+++ src/app/components/About.jsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+
+const AboutSection = () => {
+    return (
+        <section className="text-white" id="about">
+            <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+                <Image
+                    src="/images/devman.jpg"
+                    width={700}
+                    height={700}
+                    alt="Developer Image"
+                />
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+                    <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+                    <p className="text-base lg:text-lg">
+                        I'm a seasoned backend developer specializing in architecting
+                        scalable solutions. With expertise in Java, Spring Boot, and
+                        database design, I excel in building high-performance APIs and
+                        microservices. I'm committed to mentoring teams, staying abreast of
+                        industry trends, and delivering reliable, maintainable code to drive
+                        project success.
+                    </p>
+                    <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4">
+                        <div>
+                            <h3 className="text-2xl font-semibold mb-2">Skills</h3>
+                            <ul className="list-disc pl-4">
+                                <li>Java</li>
+                                <li>Spring Framework</li>
+                                <li>Spring Boot</li>
+                                <li>Microservicios</li>
+                                <li>Testing</li>
+                                <li>DevOps</li>
+                                <li>APIs RESTful</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3 className="text-2xl font-semibold mb-2">Education</h3>
+                            <ul className="list-disc pl-4">
+                                <li>Enterprise IT</li>
+                                <li>University of Costa Rica, Golfito.</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3 className="text-2xl font-semibold mb-2">Certifications</h3>
+                            <ul className="list-disc pl-4">
+                                <li>Spring Professional Certification</li>
+                                <li>AWS Certified Solutions Architect</li>
+                                <li>Docker Certified Associate</li>
+                                <li>Certified Information Systems Security Professional</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,22 @@
-import startSection from "./components/startSection";
+import StartSection from "./components/startSection";
 import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Header />
       <div className="container mt-24 mx-auto px-12 py-4">
-        <startSection />
+        <StartSection />
         <hr></hr>
+        <About />
+        <hr></hr>
+        <Projects />
+        <hr></hr>
+        <Contact />
       </div>
       <Footer />
     </main>
