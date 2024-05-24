978b60e2435522880cf77c63dfe9f8f4c3dbe082
LopezS7 <samirisraellopez21@gmail.com>
2024-05-23 22:53:23 -0600

    Se agregan imagenes respectivas para mostar en cada proyecto extraido de los repositorios de git, además de mejorar el diseño y la forma en que se muestran en pagina

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useState, useEffect } from "react";
 import { getRepositories } from "../utils/gitConnection";
 import ProjectCard from "./ProjectCard";
@@ -10,11 +9,35 @@ const Projects = () => {
     useEffect(() => {
         const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
         getRepositories(token)
-            .then(data => {
-                setRepositories(data);
+            .then((data) => {
+                
+                const repositoriosAMostrar = [
+                    { nombre: "BabyCareClinic", imagen: "/babycareclinic.jpg" },
+                    { nombre: "QuickBite", imagen: "/quickbite.jpg" },
+                    { nombre: "Chat-ClientServer", imagen: "/chat-client.jpg" },
+                    { nombre: "Pharmacy", imagen: "/healthPharmacy.jpg" },
+                    { nombre: "TicoTacoSur", imagen: "/TicoTaco.jpg" },
+                    { nombre: "CarAgency", imagen: "/caragency.jpg" },
+                ];
+                const reposToShow = data.filter((repo) =>
+                    repositoriosAMostrar.some(
+                        (repoAMostrar) => repoAMostrar.nombre === repo.name
+                    )
+                );
+                setRepositories(
+                    reposToShow.map((repo) => {
+                        const repoAMostrar = repositoriosAMostrar.find(
+                            (r) => r.nombre === repo.name
+                        );
+                        return {
+                            ...repo,
+                            imagenUrl: `/projects/${repoAMostrar.imagen}`,
+                        };
+                    })
+                );
             })
-            .catch(error => {
-                console.error('Error al obtener los repositorios:', error);
+            .catch((error) => {
+                console.error("Error al obtener los repositorios:", error);
             });
     }, []);
 
@@ -23,7 +46,6 @@ const Projects = () => {
             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
                 My Projects
             </h2>
-
             <ul className="grid md:grid-cols-3 gap-8 md:gap-12">
                 {repositories.map((repo, index) => (
                     <li key={index}>
@@ -31,7 +53,7 @@ const Projects = () => {
                             key={repo.id}
                             title={repo.name}
                             description={repo.description}
-                            imgUrl={repo.owner.avatar_url}
+                            imgUrl={repo.imagenUrl}
                             gitUrl={repo.html_url}
                             previewUrl={repo.homepage}
                         />
